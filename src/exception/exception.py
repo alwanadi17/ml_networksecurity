595bffc02a1e213bd930b7273d305524c3db3094
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2026-01-24 02:20:51 +0700

    chore: cleanup ignored files
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -4,15 +4,16 @@ import traceback
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
+    code = exc_tb.tb_frame.f_code.co_name
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
-    return error_message
+    return error_message, code
 
-class CustomException(Exception):
+class NetworkSecurityException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message, self.code = error_message_detail(error_message, error_detail=error_detail)
         self.formatted_traceback = traceback.format_stack()
 
     def __str__(self):
